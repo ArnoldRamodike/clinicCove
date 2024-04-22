8279a40a4b05f2e44ebb5bd3258001748ec91253
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-22 13:56:55 +0200

    code scraps and materials

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,37 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
+    const Menu=[
+        {
+            id: 1,
+            name: 'Home',
+            path: '/',
+        },
+        {
+            id: 2,
+            name: 'Explore',
+            path: '/explore',
+        },
+        {
+            id: 3,
+            name: 'Contact Us',
+            path: '/Contact',
+        },
+    ]
   return (
-    <div>
-         <Image src='/logo.svg' alt="logo" width={180} height={180}/>
+    <div className='flex items-center justify-between p-4 shadow-sm'>
+        <div className="flex items-center gap-10">
+            <Image src='/logo.svg' alt="logo" width={180} height={180}/>
+            <ul className='md:flex gap-8 hidden'>
+                {Menu.map((item, index) => (
+                    <Link href={item.path} className='hover:text-primary hover:scale-105 transition-all ease-in-out cursor-pointer' key={index}>{item.name}</Link>
+                ))}
+            </ul>
+        </div>
+        <Button>Get Started</Button>
     </div>
   )
 }
