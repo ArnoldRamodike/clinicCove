0e9d2bca633e89ac20434068abff43fdcf55b150
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-25 16:08:40 +0200

    Init Pages and todo

---
+++ app/(route)/details/_components/DoctorListSuggestions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorListSuggestions = () => {
+  return (
+    <div className='p-5 '>
+        <h2 className='text-center mt-2 font-bold text-2xl'>Doctors <span className='text-primary'>Suggestions</span> </h2>
+    </div>
+  )
+}
+
+export default DoctorListSuggestions
